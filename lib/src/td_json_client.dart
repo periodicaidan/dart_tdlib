39b459c83347ebb7eaaea1c8c04c122d386f3f52
periodicaidan <periodicaidan@gmail.com>
2019-11-20 22:18:43 -0600

    Refactored JsonClient and some other parts of td_json_client.dart

--- lib/src/td_json_client.dart
+++ lib/src/td_json_client.dart
@@ -1,18 +1,18 @@
-/**
- * /lib/src/td_json_client.dart
- *
- * C- and Dart-side typedefs for each of the functions in
- * td/telegram/td_json_client.h. The C versions are in snake_case and are
- * prefixed by "td_". In some cases the C and Dart typedefs have the same
- * signature; both are kept for the sake of consistency.
- *
- * This file also exports a class [JsonClient], which wraps the td_json_client
- * functions in an interface that is safe, easy-to-use, and idiomatically Dart.
- */
+/// /lib/src/td_json_client.dart
+///
+/// C- and Dart-side typedefs for each of the functions in
+/// td/telegram/td_json_client.h. The C versions are in snake_case and are
+/// prefixed by "td_". In some cases the C and Dart typedefs have the same
+/// signature; both are kept for the sake of consistency.
+///
+/// This file also exports a class [JsonClient], which wraps the td_json_client
+/// functions in an interface that is safe, easy-to-use, and idiomatically Dart.
 
-import "dart:ffi";
 import "dart:convert";
+import "dart:ffi";
+
 import "package:ffi/ffi.dart";
+
 import "utils.dart";
 
 /// Creates a new instance of TDLib client.
@@ -48,46 +48,62 @@ typedef void JsonClientDestroy(Pointer<Void> client);
 /// Represents a Telegram client that sends and receives JSON data.
 class JsonClient {
   Pointer<Void> _client;
-  DynamicLibrary _dylib;
+
   // If the client is inactive (if [destroy] has been called), further calls
-  // to this class' methods will fail with an assertion error
+  // to this class' methods will fail
   bool active;
+
+  // Private pointers to native functions, so they don't have to be looked up
+  // for each API call
+  JsonClientCreate _jsonClientCreate;
+  JsonClientSend _jsonClientSend;
+  JsonClientReceive _jsonClientReceive;
+  JsonClientExecute _jsonClientExecute;
+  JsonClientDestroy _jsonClientDestroy;
 
   JsonClient.create(String dlDir) {
     // Get the path to the td_json_client dynamic library
     final dlPath = platformPath(dlDir);
 
-    _dylib = DynamicLibrary.open(dlPath);
+    final dylib = DynamicLibrary.open(dlPath);
 
-    // Get the td_json_client_create function from the DL and create a client
-    final jsonClientCreate = _dylib
-        .lookupFunction<td_json_client_create, JsonClientCreate>("td_json_client_create");
+    // Get the td_json_client_create function from the dylib and create a client
+    _jsonClientCreate = dylib
+      .lookupFunction<td_json_client_create, JsonClientCreate>("td_json_client_create");
+    _jsonClientSend = dylib
+      .lookupFunction<td_json_client_send, JsonClientSend>("td_json_client_send");
+    _jsonClientReceive = dylib
+      .lookupFunction<td_json_client_receive, JsonClientReceive>("td_json_client_receive");
+    _jsonClientDestroy = dylib
+      .lookupFunction<td_json_client_destroy, JsonClientDestroy>("td_json_client_destroy");
+    _jsonClientExecute = dylib
+      .lookupFunction<td_json_client_execute, JsonClientExecute>("td_json_client_execute");
 
-    _client = jsonClientCreate();
+    _client = _jsonClientCreate();
     active = true;
+  }
+
+  /// If the client is not [active] then this throws an exception.
+  void assertActive() {
+    if (!active) {
+      throw Exception("Telegram JSON client has been closed.");
+    }
   }
 
   /// Send a request to the Telegram API
   /// This is an async version of [execute], which the TDLib docs don't make
   /// immediately clear :p
   void send(Map<String, dynamic> request) {
-    assert (active);
+    assertActive();
     var reqJson = json.encode(request);
-    final jsonClientSend = _dylib
-        .lookupFunction<td_json_client_send, JsonClientSend>("td_json_client_send");
-
-    jsonClientSend(_client, Utf8.toUtf8(reqJson));
+    _jsonClientSend(_client, Utf8.toUtf8(reqJson));
   }
 
   /// Receive the API's response
   Map<String, dynamic> receive([double timeout = 2.0]) {
-    assert (active);
-    final jsonClientReceive = _dylib
-        .lookupFunction<td_json_client_receive, JsonClientReceive>("td_json_client_receive");
-
-    final response = jsonClientReceive(_client, timeout);
+    assertActive();
+    final response = _jsonClientReceive(_client, timeout);
     final resString = Utf8.fromUtf8(response);
-
     return json.decode(resString);
   }
 
@@ -96,22 +112,16 @@ class JsonClient {
   /// error along the lines of "Function can't be executed synchronously"), use
   /// [send] instead.
   Map<String, dynamic> execute(Map<String, dynamic> request) {
-    assert (active);
-    final jsonClientExecute = _dylib
-        .lookupFunction<td_json_client_execute, JsonClientExecute>("td_json_client_execute");
-
-    final result = jsonClientExecute(_client, Utf8.toUtf8(json.encode(request)));
+    assertActive();
+    final result = _jsonClientExecute(_client, Utf8.toUtf8(json.encode(request)));
     var resJson = Utf8.fromUtf8(result);
     return json.decode(resJson);
   }
 
   /// Destroy the client
-  destroy() {
-    assert (active);
-    final jsonClientDestroy = _dylib
-        .lookupFunction<td_json_client_destroy, JsonClientDestroy>("td_json_client_destroy");
-
-    jsonClientDestroy(_client);
+  void destroy() {
+    assertActive();
+    _jsonClientDestroy(_client);
     active = false;
   }
 }
